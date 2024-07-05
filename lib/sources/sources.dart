230bf68d87068d4a03cfac0fd7aa3a98d8afed1c
Hasuki <denzfordev@gmail.com>
2024-07-05 16:55:18 +0700

    Update sources.dart

--- lib/sources/sources.dart
+++ lib/sources/sources.dart
@@ -1,7 +1,7 @@
 library Sources;
 
-import 'platform_sources/platform_web.dart' if (dart.library.io) 'platform_io.dart';
-export 'url_sources/url_strategy.dart' if (dart.library.html) 'url_strategy_web.dart';
+import 'platform_sources/platform_web.dart' if (dart.library.io) 'platform_sources/platform_io.dart';
+export 'url_sources/url_strategy.dart' if (dart.library.html) 'url_sources/url_strategy_web.dart';
 
 part 'platform_sources/platform_source.dart';
 part 'url_sources/url_source.dart';
